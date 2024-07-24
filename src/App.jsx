6eae5f814c9ed5697200250c2214150eefefa3bf
swastideep maharana <swastideep67maharana@gmail.com>
2024-07-24 21:05:40 +0530

    #added img in explore-menu

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,21 @@
-import React from 'react'
-import Navbar from './components/Navbar/Navbar'
+import React from "react";
+import Navbar from "./components/Navbar/Navbar";
+import { Route, Routes } from "react-router-dom";
+import Cart from "./pages/Cart/Cart";
+import PlaceOrder from "./pages/PlaceOrder/PlaceOrder";
+import Home from "./pages/Home/Home";
 
 const App = () => {
   return (
-    <div className='app'>
+    <div className="app">
       <Navbar />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/cart" element={<Cart />} />
+        <Route path="/placeorder" element={<PlaceOrder />} />
+      </Routes>
     </div>
-  )
-}
+  );
+};
 
-export default App
+export default App;
